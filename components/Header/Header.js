e178b4310dc15f48736f009387cbd7ae03cdc2eb
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-09-14 12:42:46 +0100

    header v1.

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,5 +1,27 @@
 import React from 'react'
 import styles from '../Header/header.module.css'
+import Image from 'next/image'
 export default function Header() {
-    return <div className={styles.header}> navbar</div>
+    return (
+        <div className={styles.header}>
+            <div>
+                {' '}
+                <Image
+                    src="/Logo.svg"
+                    width={111}
+                    height={37}
+                    alt="logo"
+                    className={styles.logo}
+                />
+            </div>
+
+            <div className={styles.menuItems}>
+                <div>Home</div>
+                <div>Our mission</div>
+                <div>Places</div>
+                <div>Team</div>
+            </div>
+            <button className={styles.applyButton}>Apply</button>
+        </div>
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -4,5 +4,9 @@ import Header from '../components/Header/Header'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-    return <Header />
+    return (
+        <div>
+            <Header />
+        </div>
+    )
 }
