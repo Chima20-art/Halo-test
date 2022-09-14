1915e406b265cbe2c1e737ebe92cacab661e1f49
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-09-14 20:23:05 +0100

    css.

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
     return (
-        <div>
+        <div className={styles.container}>
             <Header />
             <Search />
         </div>
