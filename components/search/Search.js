0b27edfe5861d0ed1e125e3396f928616bfd6573
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-09-14 20:22:49 +0100

    created search component.

--- components/search/Search.js
+++ components/search/Search.js
@@ -1,7 +1,46 @@
+import Image from 'next/image'
 import React from 'react'
 import Header from '../Header/Header'
 import styles from '../search/search.module.css'
 
 export default function Search() {
-    return <div className={styles.searchContainer}>it s search</div>
+    return (
+        <div className={styles.searchContainer}>
+            <div className={styles.contactForm}>
+                <img
+                    src="/Headline.svg"
+                    alt="headline"
+                    className={styles.headline}
+                />
+
+                <p className={styles.paragraph}>
+                    The scale of the challenges facing our planet can seem
+                    daunting, but we can all do something.
+                </p>
+                <div className={styles.searchDiv}>
+                    <div className={styles.inputDiv}>
+                        <Image
+                            src="/location.svg"
+                            width={14}
+                            height={20}
+                            alt="location icon"
+                        />
+                        <input
+                            type="text"
+                            className={styles.input}
+                            id="lname"
+                            name="lname"
+                            placeholder="Find the place to help"
+                        />
+                    </div>
+
+                    <button className={styles.searchButton}>SEARCH</button>
+                </div>
+            </div>
+            <div className={styles.parot}>
+                <img src="/Parrot.svg" className={styles.parotImg}></img>
+                <img src="/card.svg" className={styles.card} />
+            </div>
+        </div>
+    )
 }
