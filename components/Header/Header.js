076ef374390e14bfbaf7ceb5ea03637a9637482f
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-09-14 15:38:37 +0100

    fixed header padding.

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,24 +4,26 @@ import Image from 'next/image'
 
 export default function Header() {
     return (
-        <div className={styles.header}>
-            {' '}
-            <Image
-                src="/Logo.svg"
-                width={111}
-                height={37}
-                alt="logo"
-                className={styles.logo}
-            />
-            <div className={styles.menuItems}>
-                <div>Home</div>
-                <div>Our mission</div>
-                <div>Places</div>
-                <div>Team</div>
-            </div>
-            <button className={styles.applyButton}>Apply</button>
-            <div className={styles.menu}>
-                <Image src="/menu.svg" width={22} height={17} alt="menu" />
+        <div className={styles.headerContainer}>
+            <div className={styles.header}>
+                {' '}
+                <Image
+                    src="/Logo.svg"
+                    width={111}
+                    height={37}
+                    alt="logo"
+                    className={styles.logo}
+                />
+                <div className={styles.menuItems}>
+                    <div>Home</div>
+                    <div>Our mission</div>
+                    <div>Places</div>
+                    <div>Team</div>
+                </div>
+                <button className={styles.applyButton}>Apply</button>
+                <div className={styles.menu}>
+                    <Image src="/menu.svg" width={22} height={17} alt="menu" />
+                </div>
             </div>
         </div>
     )
