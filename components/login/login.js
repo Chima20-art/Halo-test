2e9291cd165edf075227d2e6503493802ac706ad
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-09-15 17:09:51 +0100

    added login comp.

---
+++ components/login/login.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './login.module.css'
+
+export default function Login() {
+    return (
+        <div className={styles.loginContainer}>
+            <div className={styles.loginText}>
+                <p className={styles.title}>Get started today!</p>
+                <p className={styles.text}>
+                    Learn more about how you can save our planet's nature. From
+                    smart consumption to switching to renewable energy, each of
+                    us can do our part to save the planet.{' '}
+                </p>
+            </div>
+            <div className={styles.loginForm}>
+                <h1 className={styles.loginTitle}>Log In </h1>
+                <input className={styles.input} placeholder="name" />
+                <input className={styles.input} placeholder="email" />
+                <button className={styles.loginButton}> Book a demo</button>
+            </div>
+        </div>
+    )
+}
