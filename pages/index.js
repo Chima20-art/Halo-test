b13c937405c7cf9edfb0dbc34b32cdab369973ed
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-09-15 17:10:06 +0100

    fix.

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,14 @@ import Header from '../components/Header/Header'
 import Search from '../components/search/Search'
 import Team from '../components/team/Team'
 import styles from '../styles/Home.module.css'
+import Login from '../components/login/login'
 
 export default function Home() {
     return (
         <div className={styles.container}>
             <Search />
             <Team />
+            <Login />
         </div>
     )
 }
