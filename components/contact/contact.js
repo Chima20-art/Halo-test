bdab2fa4e93cb1d1599ad37dfd49f92ba6b63c37
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-09-16 18:31:00 +0100

    fixed contact for mobile.

--- components/contact/contact.js
+++ components/contact/contact.js
@@ -3,6 +3,20 @@ import styles from './contact.module.css'
 
 export default function Contact() {
     const [openAnswer, setOpenAnswer] = useState(true)
+    var questions = [
+        {
+            question: 'What can I do to protect our planet?',
+            answer: ' Not to make an open fire in nature and to clean up litter; not to pollute open water bodies; to switch to alternative energy sources; to reduce the use of non-renewable resources ',
+        },
+        {
+            question: 'How to save nature ecology?',
+            answer: ' Not to make an open fire in nature and to clean up litter; not to pollute open water bodies; to switch to alternative energy sources; to reduce the use of non-renewable resources ',
+        },
+        {
+            question: 'What is nature conservation?',
+            answer: ' Not to make an open fire in nature and to clean up litter; not to pollute open water bodies; to switch to alternative energy sources; to reduce the use of non-renewable resources ',
+        },
+    ]
 
     return (
         <div className={styles.contactContainer}>
@@ -17,81 +31,32 @@ export default function Contact() {
                 <button className={styles.contactButton}>Contact us</button>
             </div>
             <div className={styles.contactQuestions}>
-                <div className={styles.questionDiv}>
-                    <div className={styles.question}>
-                        <div className={styles.questionTitle}>
-                            What can I do to protect our planet?
-                        </div>
-                        <div
-                            className={styles.plusIcon}
-                            onClick={() => setOpenAnswer(!openAnswer)}
-                        >
-                            {openAnswer ? (
-                                <img src="/plusButton.svg" />
-                            ) : (
-                                <img src="/minusButton.svg" />
+                {questions.map((item) => {
+                    return (
+                        <div className={styles.questionDiv}>
+                            <div className={styles.question}>
+                                <div className={styles.questionTitle}>
+                                    {item.question}
+                                </div>
+                                <div
+                                    className={styles.plusIcon}
+                                    onClick={() => setOpenAnswer(!openAnswer)}
+                                >
+                                    {openAnswer ? (
+                                        <img src="/plusButton.svg" />
+                                    ) : (
+                                        <img src="/minusButton.svg" />
+                                    )}
+                                </div>
+                            </div>
+                            {!openAnswer && (
+                                <div className={styles.paragraph}>
+                                    {item.answer}
+                                </div>
                             )}
                         </div>
-                    </div>
-                    {!openAnswer && (
-                        <div className={styles.paragraph}>
-                            Not to make an open fire in nature and to clean up
-                            litter; not to pollute open water bodies; to switch
-                            to alternative energy sources; to reduce the use of
-                            non-renewable resources
-                        </div>
-                    )}
-                </div>
-                <div className={styles.questionDiv}>
-                    <div className={styles.question}>
-                        <div className={styles.questionTitle}>
-                            What can I do to protect our planet?
-                        </div>
-                        <div
-                            className={styles.plusIcon}
-                            onClick={() => setOpenAnswer(!openAnswer)}
-                        >
-                            {openAnswer ? (
-                                <img src="/plusButton.svg" />
-                            ) : (
-                                <img src="/minusButton.svg" />
-                            )}
-                        </div>
-                    </div>
-                    {!openAnswer && (
-                        <div className={styles.paragraph}>
-                            Not to make an open fire in nature and to clean up
-                            litter; not to pollute open water bodies; to switch
-                            to alternative energy sources; to reduce the use of
-                            non-renewable resources
-                        </div>
-                    )}
-                </div>
-                <div className={styles.questionDiv}>
-                    <div className={styles.question}>
-                        <div className={styles.questionTitle}>
-                            What can I do to protect our planet?
-                        </div>
-                        <div
-                            className={styles.plusIcon}
-                            onClick={() => setOpenAnswer(!openAnswer)}
-                        >
-                            {openAnswer ? (
-                                <img src="/plusButton.svg" />
-                            ) : (
-                                <img src="/minusButton.svg" />
-                            )}
-                        </div>
-                    </div>
-                    {!openAnswer && (
-                        <div className={styles.paragraph}>
-                            Not to make an open fire in nature and to clean up
-                            litter; not to pollute open water bodies; to switch
-                            to alternative energy sources; to reduce the use of
-                            non-renewable resources
-                        </div>
-                    )}
-                </div>
+                    )
+                })}
             </div>
         </div>
     )
