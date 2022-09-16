a6ac90a1dd14c99898f7378720fb0229bb8dd4eb
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2022-09-16 17:17:11 +0100

    fixed footer responsivness.

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -7,19 +7,33 @@ export default function Footer() {
     return (
         <div className={styles.footerContainer}>
             <div className={styles.containerDiv}>
-                <p className={styles.bigTitle}> Contacts</p>
-                <p className={styles.description}>2019 Rootz Foundation.</p>
-                <p className={styles.description}> All rights reserved</p>
+                <p className={styles.bigTitle}> &nbsp;Contacts</p>
+                <div className={styles.wrap}>
+                    <div className={styles.description}>
+                        &nbsp;2019 Rootz Foundation.
+                    </div>
+                    <div className={styles.description}>
+                        &nbsp;All rights reserved
+                    </div>
+                </div>
             </div>
             <div className={styles.horizontalDiv}>
                 <div className={styles.containerDiv1}>
-                    <p className={styles.smallTitle}> Headerquarters</p>
-                    <p className={styles.description}>1234 Taliban </p>
-                    <p className={styles.description}>
-                        {' '}
-                        Los Angeles,La 1234567
-                    </p>
-                    <p className={styles.description}>(123) 456-7890</p>
+                    <div className={styles.div1}>
+                        <p className={styles.smallTitle}> Headerquarters</p>
+                        <p className={styles.description}>1234 Taliban, </p>
+                        <div className={styles.div3}>
+                            <div className={styles.description}>
+                                Los Angeles,
+                            </div>
+                            <div className={styles.description}>
+                                {' '}
+                                La 1234567
+                            </div>{' '}
+                        </div>
+
+                        <p className={styles.description}>(123) 456-7890</p>
+                    </div>
                 </div>
                 <div className={styles.containerDiv2}>
                     <p className={styles.smallTitle}> social media</p>
